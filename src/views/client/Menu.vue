51a3bf9283fa21bb77946d1288855f98fefedc48
ConanShin <conan.shin@sk.com>
2021-04-14 11:45:13 +0900

    use div instead of a tag

--- src/views/client/Menu.vue
+++ src/views/client/Menu.vue
@@ -9,8 +9,8 @@
         </div>
         <template v-if="isDesktop">
             <div class="menu">
-<!--                <div class="item" v-for="menu in menus" @click="redirect(menu.path)" :class="{bold: $route.path.includes(menu.path)}">{{ menu.name }}</div>-->
-                <a class="item" v-for="menu in menus" :href="'/' + menu.path" :class="{bold: $route.path.includes(menu.path)}">{{ menu.name }}</a>
+                <div class="item" v-for="menu in menus" @click="redirect(menu.path)" :class="{bold: $route.path.includes(menu.path)}">{{ menu.name }}</div>
+<!--                <a class="item" v-for="menu in menus" :href="'/' + menu.path" :class="{bold: $route.path.includes(menu.path)}">{{ menu.name }}</a>-->
             </div>
         </template>
         <template v-else>
