d5f2fa0dfe9878d9692f69d524478653b927f121
Cheolmin Conan Shin <conan.cheolmin.shin@gmail.com>
2025-03-13 23:33:02 +0900

    feat(address): google map 주소 변경

--- src/views/client/About.vue
+++ src/views/client/About.vue
@@ -20,7 +20,7 @@
         </div>
         <div class="flex-4">
             <img v-if="products[0]" :src="products[0].list_image"/>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.911291432864!2d126.72555341603122!3d37.60424672972388!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c83af94f7c97d%3A0xc8972452b885ca26!2z7ZW07JmA64us7J247YWM66as7Ja0!5e0!3m2!1sko!2skr!4v1618021299075!5m2!1sko!2skr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.792869663463!2d126.72063561304755!3d37.607033571911394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c83676edcbed5%3A0xfcf94bd1907c94d9!2z6rmA7Y-sIO2SjeustCDsm7Drnbzsm4Ag7Y287Y6Z7Yq4IOyLnO2LsA!5e0!3m2!1sko!2skr!4v1741876209921!5m2!1sko!2skr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
     </div>
     <div v-else>
@@ -39,7 +39,7 @@
             <div class="bold">Location</div>
             <div class="center">
                 <div>경기도 김포시 풍무로128 웰라움퍼펙트시티 2F 207호</div>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.911291432864!2d126.72555341603122!3d37.60424672972388!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c83af94f7c97d%3A0xc8972452b885ca26!2z7ZW07JmA64us7J247YWM66as7Ja0!5e0!3m2!1sko!2skr!4v1618021299075!5m2!1sko!2skr" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.792869663463!2d126.72063561304755!3d37.607033571911394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c83676edcbed5%3A0xfcf94bd1907c94d9!2z6rmA7Y-sIO2SjeustCDsm7Drnbzsm4Ag7Y287Y6Z7Yq4IOyLnO2LsA!5e0!3m2!1sko!2skr!4v1741876209921!5m2!1sko!2skr" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                 <div class="small">TEL 031 997 0512 FAX 031 997 0513</div>
                 <div class="small">EMAIL sun-mooninterior@naver.com</div>
             </div>
